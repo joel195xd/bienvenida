b9028ea94940dc38324524785ea2e6d692630291
Dark <joelpc1905@gmail.com>
2024-07-28 19:49:20 +0200

    update

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 //define the libraries
 const Discord = require("discord.js");
+const express = require('express');
 const client = new Discord.Client();
 const config = require("./config.json");
+
+// Configura Express para mantener el servicio activo
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor Express escuchando en el puerto ${port}`);
+});
+
 //when its ready log it
 client.on("ready", ()=>console.log("READY"));
 //define welcome "package"
